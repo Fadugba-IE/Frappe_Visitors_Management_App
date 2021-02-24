bbc05f470fe6904380d9d6cd16fb248147c7094e
Fadugba-IE <fadugba.iyanu@gmail.com>
2021-02-24 02:00:13 +0100

    playing around creating web pages

---
+++ visitors_management_app/doctype/visit_log_departure/visit_log_departure.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Iyanuoluwa and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Visit Log Departure', {
+	// refresh: function(frm) {
+
+	// }
+});
